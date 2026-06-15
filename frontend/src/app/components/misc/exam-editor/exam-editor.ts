6dad60381ca58dc53c2066404fa4348daef849db
LUC4R4T0R <luc4r4t0r@gmail.com>
2026-06-15 14:51:11 +0200

    added descriptive popover to text-layer setting

--- frontend/src/app/components/misc/exam-editor/exam-editor.ts
+++ frontend/src/app/components/misc/exam-editor/exam-editor.ts
@@ -1,7 +1,7 @@
 import {Component, EventEmitter, inject, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
 import {DateInput} from "../../inputs/date-input/date-input";
 import {UserSearchInput} from "../../inputs/user-search-input/user-search-input";
-import {NgbCollapse} from '@ng-bootstrap/ng-bootstrap';
+import {NgbCollapse, NgbPopover} from '@ng-bootstrap/ng-bootstrap';
 import {SemesterInput} from '../../inputs/semester-input/semester-input';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {IExam, ISemester, IUser} from 'e2x-exam-review-backend';
@@ -20,6 +20,7 @@ import { ShowInvalid } from '../../../directives/show-invalid/show-invalid';
     SemesterInput,
     ReactiveFormsModule,
     ShowInvalid,
+    NgbPopover,
   ],
   templateUrl: './exam-editor.html',
   styleUrl: './exam-editor.scss',
